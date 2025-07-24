257d090961fb81a56d46428ac5850bb6f56e5b50
wjlee611 <wjlee611@gmail.com>
2025-07-24 17:41:50 +0900

    fix(example): Fix example blocs

--- example/lib/presentation/multi_one_time_emitter/bloc/multi_ote_home_bloc.dart
+++ example/lib/presentation/multi_one_time_emitter/bloc/multi_ote_home_bloc.dart
@@ -1,15 +1,13 @@
 import 'package:bloc/bloc.dart';
 import 'package:bloc_mixins/bloc_mixins.dart';
-import 'package:equatable/equatable.dart';
 
 part 'multi_ote_home_event.dart';
-part 'multi_ote_home_state.dart';
 
 class MultiOTEHomeBloc extends Bloc<MultiOTEHomeEvent, void>
     with OneTimeEmitter<int> {
   int _count = 0;
 
-  MultiOTEHomeBloc() : super(const MultiOTEHomeState.init()) {
+  MultiOTEHomeBloc() : super(null) {
     on<MultiOTEHomeIncrementEvent>((event, emit) {
       _count++;
       oneTimeEmit(_count);
--- example/lib/presentation/multi_one_time_emitter/bloc/multi_ote_home_cubit.dart
+++ example/lib/presentation/multi_one_time_emitter/bloc/multi_ote_home_cubit.dart
@@ -4,7 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class MultiOTEHomeCubit extends Cubit<void> with OneTimeEmitter<int> {
   int _count = 0;
 
-  MultiOTEHomeCubit() : super(0);
+  MultiOTEHomeCubit() : super(null);
 
   void increment() {
     _count++;
