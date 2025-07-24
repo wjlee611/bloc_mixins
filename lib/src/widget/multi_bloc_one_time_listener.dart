bf2a46b5a746e0f4d7850fde249c5d0101c5763b
wjlee611 <wjlee611@gmail.com>
2025-07-24 16:36:54 +0900

    docs(multi_bloc_one_time_listener): Add comments

--- lib/src/widget/multi_bloc_one_time_listener.dart
+++ lib/src/widget/multi_bloc_one_time_listener.dart
@@ -2,7 +2,53 @@ import 'package:bloc_mixins/bloc_mixins.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+/// {@template multi_bloc_one_time_listener}
+/// Merges multiple [BlocOneTimeListener] widgets into one widget tree.
+///
+/// [MultiBlocOneTimeListener] improves the readability and eliminates the need
+/// to nest multiple [BlocOneTimeListener]s.
+///
+/// By using [MultiBlocOneTimeListener] we can go from:
+///
+/// ```dart
+/// BlocOneTimeListener<BlocA, BlocAOneTimeEmitEntity>(
+///   listener: (context, value) {},
+///   child: BlocOneTimeListener<BlocB, BlocBOneTimeEmitEntity>(
+///     listener: (context, value) {},
+///     child: BlocOneTimeListener<BlocC, BlocCOneTimeEmitEntity>(
+///       listener: (context, value) {},
+///       child: ChildA(),
+///     ),
+///   ),
+/// )
+/// ```
+///
+/// to:
+///
+/// ```dart
+/// MultiBlocOneTimeListener(
+///   listeners: [
+///     BlocOneTimeListener<BlocA, BlocAOneTimeEmitEntity>(
+///       listener: (context, value) {},
+///     ),
+///     BlocOneTimeListener<BlocB, BlocBOneTimeEmitEntity>(
+///       listener: (context, value) {},
+///     ),
+///     BlocOneTimeListener<BlocC, BlocCOneTimeEmitEntity>(
+///       listener: (context, value) {},
+///     ),
+///   ],
+///   child: ChildA(),
+/// )
+/// ```
+///
+/// [MultiBlocOneTimeListener] converts the [BlocOneTimeListener] list into a
+/// tree of nested [BlocOneTimeListener] widgets.
+/// As a result, the only advantage of using [MultiBlocOneTimeListener] is
+/// improved readability due to the reduction in nesting and boilerplate.
+/// {@endtemplate}
 class MultiBlocOneTimeListener extends MultiBlocListener {
+  /// {@macro multi_bloc_one_time_listener}
   MultiBlocOneTimeListener({
     required List<BlocOneTimeListener> listeners,
     required Widget child,
